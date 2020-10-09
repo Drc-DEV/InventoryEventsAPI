cdc9225a52e90d95a8ba9e30a2dfb3c8e85524da
Drc-DEV <dracarys.pro@gmail.com>
2020-10-09 21:09:59 +0200

    Clean comments

--- src/main/java/pro/dracarys/inventoryevents/event/PlayerInventoryOpenEvent.java
+++ src/main/java/pro/dracarys/inventoryevents/event/PlayerInventoryOpenEvent.java
@@ -4,8 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
 /**
- * Fired when a player opens any other inventory than his.
- * Why not his: Couldn't figure out how to listen for Window Confirmation packets and ran out of time.
+ * Fired when a player opens any inventory.
  */
 public class PlayerInventoryOpenEvent extends PlayerInventoryEvent {
     public PlayerInventoryOpenEvent(Player player, Inventory inventory) {
